d0b6183398d5db7c20d9c0f0c0493c3d877455b0
John Goodliff <jerboa88@users.noreply.github.com>
2025-05-10 20:49:38 -0600

    Rewrite counter module for better performance, line count support, and grapheme-based string splitting

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -1,68 +1,36 @@
-let isInWord = false;
-let isInSentence = false;
-let isInParagraph = false;
-let isPendingSentenceStart = false;
+const WAS_BLANK = 0;
+const WAS_IN_PARAGRAPH = 1;
+const WAS_IN_SENTENCE = 2;
+const WAS_END_MARK = 3;
+const WAS_IN_WORD = 4;
 
+let lastCharacterState = WAS_BLANK;
+
+// Counts
 let characters = 0;
 let words = 0;
 let sentences = 0;
 let paragraphs = 0;
+let lines = 0;
+
+// Character counts
 let spaces = 0;
 let letters = 0;
 let digits = 0;
 let symbols = 0;
 
-// TODO: Add line counting
 
 function resetCounts() {
-	isInWord = false;
-	isInSentence = false;
-	isInParagraph = false;
-	isPendingSentenceStart = false;
-
+	lastCharacterState = WAS_BLANK;
 	characters = 0;
 	words = 0;
 	sentences = 0;
 	paragraphs = 0;
+	lines = 0;
 	spaces = 0;
 	letters = 0;
 	digits = 0;
 	symbols = 0;
-}
-
-function startParagraph() {
-	isInWord = true;
-	isInSentence = true;
-	isInParagraph = true;
-	isPendingSentenceStart = false;
-}
-
-function endWord() {
-	if (isInWord) {
-		isInWord = false;
-
-		words++;
-	}
-}
-
-function endSentence() {
-	endWord();
-
-	if (isInSentence) {
-		isInSentence = false;
-
-		sentences++;
-	}
-}
-
-function endParagraph() {
-	endSentence();
-
-	if (isInParagraph) {
-		isInParagraph = false;
-
-		paragraphs++;
-	}
 }
 
 function isDigit(character) {
@@ -89,57 +57,87 @@ function isNewline(character) {
 	return character === '\n';
 }
 
-function isPeriod(character) {
-	return character === '.';
-}
-
-function isSentenceEnd(character) {
+function isEndMark(character) {
 	// This is slightly faster than using `includes()` on a string, presumably because we don't need to perform case-sensitive comparisons
-	return ['?', '!'].includes(character);
+	return ['.', '?', '!'].includes(character);
 }
 
-export async function getCounts(text) {
+export async function getCounts(text, locales = 'en') {
 	const startTime = performance.timeOrigin + performance.now();
+	const graphemeSegmenter = new Intl.Segmenter(locales, {
+		granularity: 'grapheme',
+	});
+	const segmentIterator = graphemeSegmenter.segment(text)[Symbol.iterator]();
 
 	resetCounts();
 
-	for (let i = 0; i < text.length; i++) {
-		const currentCharacter = text[i];
-
+	for (const { segment: currentCharacter } of segmentIterator) {
 		characters++;
 
 		if (isLetter(currentCharacter)) {
-			startParagraph();
-
 			letters++;
-		} else if (isDigit(currentCharacter)) {
-			startParagraph();
 
+			lastCharacterState = WAS_IN_WORD;
+		} else if (isDigit(currentCharacter)) {
 			digits++;
+
+			lastCharacterState = WAS_IN_WORD;
 		} else if (isSpace(currentCharacter)) {
 			spaces++;
 
-			if (isPendingSentenceStart) {
-				endSentence();
-			} else {
-				endWord();
+			if (lastCharacterState >= WAS_IN_WORD) {
+				words++;
+
+				lastCharacterState = WAS_IN_SENTENCE;
+			} else if (lastCharacterState >= WAS_END_MARK) {
+				words++;
+				sentences++;
+
+				lastCharacterState = WAS_IN_PARAGRAPH;
 			}
-		} else if (isPeriod(currentCharacter)) {
-			// Delay recording 1 sentence until we know if this is a decimal point or an end of sentence punctuation mark
-			isPendingSentenceStart = true;
-		} else if (isSentenceEnd(currentCharacter)) {
-			endSentence();
+		} else if (
+			lastCharacterState >= WAS_IN_WORD &&
+			isEndMark(currentCharacter)
+		) {
+			symbols++;
+
+			// Delay recording a word/sentence until we know if this is a decimal point or an end of sentence punctuation mark
+			lastCharacterState = WAS_END_MARK;
 		} else if (isNewline(currentCharacter)) {
-			endParagraph();
+			if (lastCharacterState >= WAS_IN_PARAGRAPH) {
+				words++;
+				sentences++;
+				paragraphs++;
+
+				lastCharacterState = WAS_BLANK;
+			}
+
+			lines++;
 		} else {
 			symbols++;
 		}
 	}
 
-	endParagraph();
+	if (lastCharacterState >= WAS_END_MARK) {
+		words++;
+	}
+
+	if (lastCharacterState >= WAS_IN_SENTENCE) {
+		sentences++;
+	}
+
+	if (lastCharacterState >= WAS_IN_PARAGRAPH) {
+		paragraphs++;
+	}
+
+	// Count the last line if it doesn't end with a newline
+	// Skip if the input is an empty string
+	if (characters > 0) {
+		lines++;
+	}
 
 	console.debug(
-		`Processed text in ${performance.timeOrigin + performance.now() - startTime}ms`,
+		`Processed text in ${(performance.timeOrigin + performance.now() - startTime).toFixed(0)}ms`,
 	);
 
 	return {
@@ -147,6 +145,7 @@ export async function getCounts(text) {
 		words,
 		sentences,
 		paragraphs,
+		lines,
 		spaces,
 		letters,
 		digits,
