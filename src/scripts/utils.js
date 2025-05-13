321a92620eb95a4153d663379bd217a5b76b8d94
John Goodliff <jerboa88@users.noreply.github.com>
2025-05-13 17:59:36 -0600

    Add debug utility function

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,3 +1,23 @@
+/**
+ * Log a debug message to the console.
+ *
+ * @param {string} msg - The message to log.
+ */
+export function debug(msg) {
+	console.debug(
+		'%c[Tally]',
+		'color:var(--color-primary);background:var(--color-base-1);font-family:var(--font-family-header);font-weight:var(--font-weight-bold);',
+		msg,
+	);
+}
+
+/**
+ * Debounce a function to limit how often it can be called.
+ *
+ * @param {(...args: any[]) => void} fn - The function to debounce. Must be a function that takes any number of arguments and returns void.
+ * @param {number} [delay=50] - The delay in milliseconds.
+ * @returns {(...args: any[]) => void} - The debounced function.
+ */
 export function debounce(fn, delay = 50) {
 	let timeoutId;
 
