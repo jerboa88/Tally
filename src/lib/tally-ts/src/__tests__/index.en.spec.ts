2501c367f44eb3b25d6b341168e662e25764e864
John Goodliff <jerboa88@users.noreply.github.com>
2025-10-17 14:58:16 -0600

    test(tally-ts): rewrite tests using Bun's test runner

---
+++ src/lib/tally-ts/src/__tests__/index.en.spec.ts
@@ -0,0 +1,245 @@
+import { runTests } from './utils.ts';
+
+const locale = 'en' as const;
+const testCases = [
+	{
+		label: 'empty string',
+		input: '',
+		expectedGraphemes: {
+			total: 0,
+			by: {
+				spaces: {
+					total: 0,
+				},
+				letters: {
+					total: 0,
+				},
+				digits: {
+					total: 0,
+				},
+				punctuation: {
+					total: 0,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 0,
+				},
+				lines: {
+					total: 0,
+				},
+			},
+		},
+		expectedWords: {
+			total: 0,
+		},
+		expectedSentences: {
+			total: 0,
+		},
+	},
+	{
+		label: '1 word',
+		// https://tatoeba.org/en/sentences/show/1437
+		// CC BY 2.0 FR (https://creativecommons.org/licenses/by/2.0/fr/)
+		input: 'Congratulations!',
+		expectedGraphemes: {
+			total: 16,
+			by: {
+				spaces: {
+					total: 0,
+				},
+				letters: {
+					total: 15,
+				},
+				digits: {
+					total: 0,
+				},
+				punctuation: {
+					total: 1,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 1,
+				},
+				lines: {
+					total: 1,
+				},
+			},
+		},
+		expectedWords: {
+			total: 1,
+		},
+		expectedSentences: {
+			total: 1,
+		},
+	},
+	{
+		label: '1 sentence',
+		// https://tatoeba.org/en/sentences/show/1308
+		// CC BY 2.0 FR (https://creativecommons.org/licenses/by/2.0/fr/)
+		input: 'I miss you.',
+		expectedGraphemes: {
+			total: 11,
+			by: {
+				spaces: {
+					total: 2,
+				},
+				letters: {
+					total: 8,
+				},
+				digits: {
+					total: 0,
+				},
+				punctuation: {
+					total: 1,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 1,
+				},
+				lines: {
+					total: 1,
+				},
+			},
+		},
+		expectedWords: {
+			total: 3,
+		},
+		expectedSentences: {
+			total: 1,
+		},
+	},
+	{
+		label: '1 sentence w/ punctuation',
+		// https://tatoeba.org/en/sentences/show/1360
+		// CC BY 2.0 FR (https://creativecommons.org/licenses/by/2.0/fr/)
+		input: 'Back in high school, I got up at 6 a.m. every morning.',
+		expectedGraphemes: {
+			total: 54,
+			by: {
+				spaces: {
+					total: 11,
+				},
+				letters: {
+					total: 38,
+				},
+				digits: {
+					total: 1,
+				},
+				punctuation: {
+					total: 4,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 1,
+				},
+				lines: {
+					total: 1,
+				},
+			},
+		},
+		expectedWords: {
+			total: 11,
+		},
+		expectedSentences: {
+			total: 1,
+		},
+	},
+	{
+		label: '2 sentences',
+		// https://tatoeba.org/en/sentences/show/1370
+		// CC BY 2.0 FR (https://creativecommons.org/licenses/by/2.0/fr/)
+		input: "I can't tell her now. It's not that simple anymore.",
+		expectedGraphemes: {
+			total: 51,
+			by: {
+				spaces: {
+					total: 9,
+				},
+				letters: {
+					total: 38,
+				},
+				digits: {
+					total: 0,
+				},
+				punctuation: {
+					total: 4,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 1,
+				},
+				lines: {
+					total: 1,
+				},
+			},
+		},
+		expectedWords: {
+			total: 10,
+		},
+		expectedSentences: {
+			total: 2,
+		},
+	},
+	{
+		label: '2 paragraphs',
+		// https://www.un.org/en/about-us/universal-declaration-of-human-rights
+		input:
+			'Everyone has the right to life, liberty and security of person.\n\nNo one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms.',
+		expectedGraphemes: {
+			total: 178,
+			by: {
+				spaces: {
+					total: 30,
+				},
+				letters: {
+					total: 142,
+				},
+				digits: {
+					total: 0,
+				},
+				punctuation: {
+					total: 4,
+				},
+				symbols: {
+					total: 0,
+				},
+			},
+			related: {
+				paragraphs: {
+					total: 2,
+				},
+				lines: {
+					total: 3,
+				},
+			},
+		},
+		expectedWords: {
+			total: 32,
+		},
+		expectedSentences: {
+			total: 2,
+		},
+	},
+];
+
+runTests(locale, testCases);
